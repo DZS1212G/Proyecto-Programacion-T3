eafd1d1f78f19c67ada61cb1fe6176cf8a520123
DZS1212 <diego.zapatero.sobaco@gregoriofer.com>
2026-03-16 13:41:17 +0100

    Empiece

---
+++ JavaApplication15/src/Aplicacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author zapsobdi
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        GUIAgenda gui =new GUIAgenda();
+        gui.setVisible(true);
+    }
+}
---
+++ JavaApplication15/src/Contacto.java
@@ -0,0 +1,48 @@
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author zapsobdi
+ */
+public class Contacto {
+
+    private String nombre;
+    private int telefono;
+    private LocalDate fechaRegistro;
+
+    public Contacto(String nombre, int telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.fechaRegistro = LocalDate.now();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+}
